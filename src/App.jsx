4894676cca429b29fef67c70794abc65d6aa6696
Abdullah Al Faiyaz <abdullah-al.faiyaz@stud.uni-bamberg.de>
2024-09-22 01:07:56 +0200

    Knowledge cafe work starts

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./components/Header/Header";
+import Blogs from "./components/Blogs/Blogs";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header> </Header>
+      <Blogs> </Blogs>
+    </>
+  );
+}
+
+export default App;
